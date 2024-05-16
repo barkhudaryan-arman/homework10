8983b416f2227183e24a364ae0eb2603082c9fe6
barkhudaryan-arman <armanik147@gmail.com>
2024-05-16 14:07:55 +0300

    Проверьте ДЗ!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1
+        {
+            String firstName = "Ivanov";
+            String middleName = "Ivan";
+            String lastName = "Ivanovich";
+            String fullName = "Ivanov Ivan Ivanovich";
+            System.out.println("Ф. И. О. сотрудника — " + fullName);
+        }
+        //Задача 2
+        {
+            String fullName = "Ivanov Ivan Ivanovich";
+            System.out.println("Ф. И. О. сотрудника — " + fullName.toUpperCase());
+        }
+        //Задача 3
+        {
+            String fullName = "Иванов Семён Семёнович";
+            fullName = fullName.replace("ё", "е");
+            System.out.println("Ф. И. О. сотрудника — " + fullName);
+        }
     }
 }
